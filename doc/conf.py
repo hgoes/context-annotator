0f2824999072cf893b42ef0d0879b92bf00aa76e
Henning Günther <h.guenther@tu-bs.de>
2009-07-20 12:27:42 +0000

    Added matplotlib link for intersphinx documentation

--- doc/conf.py
+++ doc/conf.py
@@ -197,4 +197,5 @@ latex_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'http://docs.python.org/': None}
+intersphinx_mapping = {'http://docs.python.org/': None,
+                       'http://matplotlib.sourceforge.net': None }
