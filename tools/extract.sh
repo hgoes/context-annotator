9e7c5bf786a69769dc2e46817d4e153ee45869a8
Stefan Windt <stefan@windt.eu>
2010-02-18 15:21:16 +0000

    * remove empty files

--- tools/extract.sh
+++ tools/extract.sh
@@ -4,5 +4,10 @@ CONTEXTS=("PSI" "PST" "PLY" "PWA" "PSU" "PCY" "HHO" "HTA" "HTM" "SKE");
 for context in ${CONTEXTS[@]}
 do
  echo "Processing ${context}...";
- cat ${inputfile} | grep ${context} > ${inputfile}_${context}.log;
+ outputfile=${inputfile}_${context}.log;
+ cat ${inputfile} | grep ${context} > $outputfile
+ filesize=$(stat -c%s "${outputfile}");
+ if [ ${filesize} == 0 ]; then
+   rm -f ${outputfile};
+ fi
 done
