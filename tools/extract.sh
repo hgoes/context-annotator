1c1ff17b7be1f8c2cdf9eb723a411f89becfdff1
Firas El Simrany <f.el-simrany@tu-bs.de>
2010-02-24 20:47:38 +0000

    script for grepping and cutting von conversion files.

--- tools/extract.sh
+++ tools/extract.sh
@@ -4,10 +4,12 @@ CONTEXTS=("PSI" "PST" "PLY" "PWA" "PSU" "PCY" "HHO" "HTA" "HTM" "SKE");
 for context in ${CONTEXTS[@]}
 do
  echo "Processing ${context}...";
- outputfile=${inputfile}_${context}.log;
+ outputfile=${context};
  cat ${inputfile} | grep ${context} > $outputfile
  filesize=$(stat -c%s "${outputfile}");
  if [ ${filesize} == 0 ]; then
    rm -f ${outputfile};
+ else 
+  sed '1,100d' ${outputfile} > temp; sed -n -e :a -e '1,100!{P;N;D;};N;ba' temp > $outputfile;rm temp;
  fi
 done
